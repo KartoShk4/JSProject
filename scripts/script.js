8bb5f521a6b91cabaa477febe36307f5611c65a6
Evgeny.M <trisemerka@gmail.com>
2024-11-25 13:54:31 +0500

    refresh files and added corrections

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,14 @@
 // Нашли кнопку и иконку на странице
 const dropdownToggle = document.querySelector('.btn-toggle');
 const chevronIcon = dropdownToggle.querySelector('.fa-chevron-right');
-const btnToggle = document.getElementById('btn-toggle');
 
-// Создали функцию, которая при нажатии на кнопку будет использовать стиль CSS и поворачивать стрелочку
+
+// Функция при нажатии на кнопку будет использовать стиль CSS и поворачивать стрелочку
 dropdownToggle.addEventListener('click', () => {
     chevronIcon.classList.toggle('rotate');
 });
+
+// Убираем border-radius-bottom при клике
+dropdownToggle.addEventListener('click', () => {
+    dropdownToggle.classList.toggle('border-radius-bottom')
+})
